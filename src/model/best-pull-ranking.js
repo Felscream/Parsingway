9f7bcc1f18e4d029b319cb03b8b239082e3e3c6c
Felscream <xblackfieldx@gmail.com>
2024-08-20 15:15:53 +0200

    Removed unused method

--- src/model/best-pull-ranking.js
+++ src/model/best-pull-ranking.js
@@ -1,12 +1,6 @@
-import objectHash from "object-hash";
-
 export default class BestPullRanking {
   constructor(pull, ranking) {
     this.pull = pull;
     this.ranking = ranking;
-  }
-
-  getHash() {
-    return objectHash(this);
   }
 }
